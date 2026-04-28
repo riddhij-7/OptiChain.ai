e04329ef90a245d68b4f4b4082614944afb6972a
Anishj2029 <anishjoshi2029@gmail.com>
2026-04-28 18:39:26 +0530

    production chnages

--- frontend/src/services/geminiService.js
+++ frontend/src/services/geminiService.js
@@ -1,5 +1,5 @@
 const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
-const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${GEMINI_API_KEY}`;
+const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=${GEMINI_API_KEY}`;
 
 async function callGemini(prompt) {
   const response = await fetch(GEMINI_URL, {
